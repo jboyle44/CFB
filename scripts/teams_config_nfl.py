62e5665bd597f52f805dcf63ea4cd23f4c772b91
jboyle44 <joseph.boyle44@gmail.com>
2026-09-02 15:38:30 -0400

    Add NFC East team registry for NFL scraper

---
+++ scripts/teams_config_nfl.py
@@ -0,0 +1,11 @@
+"""
+NFC East team registry. Ourlads NFL URLs use a bare team abbreviation:
+https://www.ourlads.com/nfldepthcharts/depthchart/{ourlads_abbr}
+"""
+
+NFL_TEAMS = {
+    "dal": {"display_name": "Dallas Cowboys", "ourlads_abbr": "DAL"},
+    "nyg": {"display_name": "New York Giants", "ourlads_abbr": "NYG"},
+    "phi": {"display_name": "Philadelphia Eagles", "ourlads_abbr": "PHI"},
+    "was": {"display_name": "Washington Commanders", "ourlads_abbr": "WAS"},
+}
