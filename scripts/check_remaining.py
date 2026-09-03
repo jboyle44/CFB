d3ad4938943bb142982953cd3e2d6205a8db91a7
jboyle44 <joseph.boyle44@gmail.com>
2026-09-03 07:28:18 -0400

    cleanup

--- scripts/check_remaining.py
+++
@@ -1,6 +0,0 @@
-import os, requests
-api_key = os.environ.get("CFBD_API_KEY")
-headers = {"Authorization": f"Bearer {api_key}", "Accept": "application/json"}
-resp = requests.get("https://api.collegefootballdata.com/teams/fbs", headers=headers, timeout=20)
-with open('../remaining_result.txt', 'w') as f:
-    f.write(f"Remaining: {resp.headers.get('X-CallLimit-Remaining')}")
