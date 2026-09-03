c29aa1a756e29bf630b8d03aa300b22fb2ef7051
jboyle44 <joseph.boyle44@gmail.com>
2026-09-03 07:25:59 -0400

    cleanup

--- scripts/test_3teams.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(0, '.')
-from build_depth_chart import build
-for team_key in ['ohio-state', 'oregon', 'texas']:
-    print(f"=== Building {team_key} ===")
-    build(team_key, f'../depth_chart_data/{team_key}.json')
