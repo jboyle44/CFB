f10a477037aa4654034b7199cad0c983972fdbda
jboyle44 <joseph.boyle44@gmail.com>
2026-09-03 07:43:53 -0400

    Test full multi-hop fix on 4 schools

---
+++ scripts/test_4schools.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, '.')
+from build_depth_chart import build
+for team_key in ['ohio-state', 'oregon', 'texas', 'indiana']:
+    print(f"=== Building {team_key} ===")
+    build(team_key, f'../depth_chart_data/{team_key}.json')
