a9c9422fc3136d4d2426590df0cb9bdd765fb5c3
jboyle44 <joseph.boyle44@gmail.com>
2026-09-02 23:02:42 -0400

    Madden 27 team ID mapping for teamcrafters.net ratings

---
+++ scripts/madden27_team_ids.py
@@ -0,0 +1,12 @@
+# Maps our internal NFL team_key (dal/nyg/phi/etc) to teamcrafters.net's
+# numeric Madden team ID. Verified against the 23-super-bowl / 082726 pages
+# (same numeric IDs across roster versions).
+MADDEN27_TEAM_IDS = {
+    "ari": 26547, "atl": 26535, "bal": 26519, "buf": 26523, "car": 26536,
+    "chi": 26543, "cin": 26520, "cle": 26521, "dal": 26539, "den": 26531,
+    "det": 26544, "gb": 26545, "hou": 26527, "ind": 26528, "jax": 26529,
+    "kc": 26532, "lv": 26533, "lac": 26534, "lar": 26548, "mia": 26524,
+    "min": 26546, "ne": 26525, "no": 26537, "nyg": 26540, "nyj": 26526,
+    "phi": 26541, "pit": 26522, "sf": 26549, "sea": 26550, "tb": 26538,
+    "ten": 26530, "was": 26542,
+}
