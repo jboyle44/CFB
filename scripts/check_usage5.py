39d5b9ef74208ea1d05f0c98bdfbc74f2c7926a4
jboyle44 <joseph.boyle44@gmail.com>
2026-09-03 07:28:17 -0400

    cleanup

--- scripts/check_usage5.py
+++
@@ -1,6 +0,0 @@
-import json, os, requests
-api_key = os.environ.get("CFBD_API_KEY")
-headers = {"Authorization": f"Bearer {api_key}", "Accept": "application/json"}
-resp = requests.get("https://api.collegefootballdata.com/info/usage", headers=headers, timeout=20)
-with open('../usage_check5_result.json', 'w') as f:
-    f.write(resp.text)
