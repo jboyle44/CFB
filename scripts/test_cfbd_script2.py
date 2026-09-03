4d1385aacf603a6c11d76582c4e23e4494ff34f8
jboyle44 <joseph.boyle44@gmail.com>
2026-09-02 22:02:40 -0400

    Test different transfer portal years

--- scripts/test_cfbd_script2.py
+++ scripts/test_cfbd_script2.py
@@ -1,23 +1,15 @@
 import json
-from scrape_cfbd_recruiting import get_recruiting_players, get_transfer_portal
+from scrape_cfbd_recruiting import get_transfer_portal
 
 result = {}
-
-try:
-    players = get_recruiting_players('Ohio State', 2025)
-    result['recruiting_players_count'] = len(players)
-    result['recruiting_sample'] = dict(list(players.items())[:5])
-except Exception as e:
-    result['recruiting_error'] = str(e)
-
-try:
-    portal = get_transfer_portal(2026)
-    result['portal_total_count'] = len(portal)
-    oh_state_transfers = {n: i for n, i in portal.items() if i.get('destination') == 'Ohio State'}
-    result['ohio_state_transfer_count'] = len(oh_state_transfers)
-    result['ohio_state_transfer_sample'] = dict(list(oh_state_transfers.items())[:5])
-except Exception as e:
-    result['portal_error'] = str(e)
+for yr in [2024, 2025, 2026]:
+    try:
+        portal = get_transfer_portal(yr)
+        result[f'year_{yr}_count'] = len(portal)
+        if len(portal) > 0:
+            result[f'year_{yr}_sample'] = dict(list(portal.items())[:3])
+    except Exception as e:
+        result[f'year_{yr}_error'] = str(e)
 
 with open('../test_cfbd_result.json', 'w') as f:
     json.dump(result, f, indent=2)
