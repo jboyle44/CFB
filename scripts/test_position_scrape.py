47c50739ec7fa3b9e4bd451634903706867c96f5
jboyle44 <joseph.boyle44@gmail.com>
2026-09-04 20:43:57 -0400

    cleanup

--- scripts/test_position_scrape.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, '.')
-from scrape_teamcrafters import get_cfb27_ratings
-
-ratings = get_cfb27_ratings(701)  # Ohio State
-with open('../position_test_result.txt', 'w') as f:
-    for name in ['jeremiah smith', 'julian sayin', 'kenyatta jackson jr.', 'terry moore', 'bo jackson']:
-        info = ratings.get(name)
-        f.write(f"{name!r}: {info}\n")
